0dcf29f01f60477b13d08e945774b63e1f207737
NinaB3 <nina.borne--casaucau@etu.estia.fr>
2025-05-26 15:18:44 +0200

    Ajout fonction color of the tenth pixel #23

--- src/features.c
+++ src/features.c
@@ -31,5 +31,14 @@ void first_pixel (char *source_path){
     unsigned char *data = NULL;
     int width=0, height =0, channel_count=0;
     read_image_data(source_path, &data, &width, &height, &channel_count);
-   printf("first_pixel: %d , %d , %d ",data[0],data[1],data[2]); 
+    printf("tenth_pixel: %d , %d , %d ",data[0],data[1],data[2]); 
 }
+
+void tenth_pixel (char *source_path){
+    /*int R,G,B;*/ 
+    unsigned char *data = NULL;
+    int width=0, height =0, channel_count=0;
+    read_image_data(source_path, &data, &width, &height, &channel_count);
+    printf("tenth_pixel: %d , %d , %d ",data[28],data[29],data[30]); 
+}
+
